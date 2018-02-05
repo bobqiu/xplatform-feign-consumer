154fbfb3b90e7a2ef99709422ae93d3ecca218b4
bobqiu <19881xy@163.com>
2018-02-05 14:39:07 +0800

    init

---
+++ src/main/java/com/qiu/feign/XplatformFeignConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.qiu.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XplatformFeignConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XplatformFeignConsumerApplication.class, args);
+	}
+}
